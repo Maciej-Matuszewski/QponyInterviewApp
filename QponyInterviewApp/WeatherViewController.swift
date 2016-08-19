4e7e9e0e08c8756e8e963c4595362931c4cd7f6b
Gr3mlin106 <maciej.matuszewski@me.com>
2016-08-19 19:36:05 +0200

    [Config] Prepare project

---
+++ QponyInterviewApp/WeatherViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherViewController.swift
+//  QponyInterviewApp
+//
+//  Created by Maciej Matuszewski on 19.08.2016.
+//  Copyright © 2016 Maciej Matuszewski. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = Constants.kColorBackground
+    }
+    
+}
